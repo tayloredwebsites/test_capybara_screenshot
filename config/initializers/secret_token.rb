0cd0de77399de8d9dc0d86695ff30b9d9690eca8
Dave Taylor <tayloredwebsites@me.com>
2012-11-22 09:21:48 -0500

    initial generated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCapybaraScreenshot::Application.config.secret_token = 'd372fb524ced2f99af24ae83a601d10114087eb1c54500c5ee6a1fdb7ed9dee1661052b84b1a6da390a3b78beb09657eefbcbf24869abc8d7b4a06fede5047ad'
